6889cd0aac43d14511a0e378bc4666412b4c5420
shai9 <dennis.shakava@student.moringaschool.com>
2022-12-05 00:59:13 +0300

    Feed the controllers with required actions

---
+++ app/api/v1/reviews_controller.rb
@@ -0,0 +1,31 @@
+module Api
+  module V1
+    class ReviewsController < ApplicationController
+      def create
+        review = Review.new(review_params)
+
+        if review.save
+          render json: ReviewSerializer.new(review).serializable_hash
+        else
+          render json: { errors: review.errors.messages }, status: 422
+        end
+      end
+      
+      def destroy
+        review = Review.find(params[:id])
+
+        if review.destroy
+          head :no_content
+        else
+          render json: { errors: review.errors.messages }, status: 422
+        end
+      end
+
+      private
+
+      def review_params
+        params.permit(:title, :description, :score, :hotel_id)
+      end
+    end
+  end
+end
