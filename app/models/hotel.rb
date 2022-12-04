fa9811e897f9c31c2c28a0727c30cba0c4c21f94
shai9 <dennis.shakava@student.moringaschool.com>
2022-12-05 00:32:16 +0300

    Create required action for hotel model

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,2 +1,15 @@
 class Hotel < ApplicationRecord
+  has_many :reviews
+
+  # Take a hotel name and convert it to a url safe slug
+  before_create :slugify
+  def slugify
+    self.slug = name.parameterize
+  end
+
+  def avg_score
+    return 0 unless reviews.size.positive?
+
+    reviews.average(:score).to_f.round(2)
+  end
 end
