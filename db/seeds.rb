c0e9d51da30efebb90fe9c5e244574ca8b3010f1
shai9 <dennis.shakava@student.moringaschool.com>
2022-12-05 00:38:56 +0300

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,82 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Seeding data!'
+
+hotels = Hotel.create([
+  { 
+    name: "Lotos Inn & Suites, Nairobi",
+    image_url: "https://scontent.fnbo16-1.fna.fbcdn.net/v/t39.30808-6/301132171_481919267277439_9221096392396575226_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=a21K293odc8AX-YQ8q5&_nc_ht=scontent.fnbo16-1.fna&oh=00_AfDXEqSEFXtL1HCthbE0q6hJ01n-O-GRvCaIFIKSfIF8lw&oe=638DBCEF"
+  }, 
+  { 
+    name: "Bahari Beach Hotel",
+    image_url: "https://design.maestroarchitects.co.ke/wp-content/uploads/2017/01/bahari-beach-hotel-logo.jpg"
+  },
+  { 
+    name: "Best Western Plus Meridian Hotel",
+    image_url: "https://www.haciendahotel-oldtown.com/files/4808/logo-glow.png" 
+  }, 
+  { 
+    name: "Villa Rossa Kempinski",
+    image_url: "https://www.conferencehotelgroup.com/img/hotels/logos/576/villarosalogo%20(3).jpg" 
+  }, 
+  { 
+    name: "Ole Sereni",
+    image_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.kenyanz.com%2Flogos%2Fsocial_media%2Flimage-19816-490-photo.png&f=1&nofb=1&ipt=64a8e157faeb710494a979d9cd621bcf8ac0da2c1cbc778c9a5f25f95e015ca0&ipo=images" 
+  }, 
+  { 
+    name: "Hilton Hotels",
+    image_url: "https://logos-world.net/wp-content/uploads/2021/02/Hilton-Emblem.png" 
+  },
+  { 
+    name: "Hunters Paradise Cottages",
+    image_url: "https://huntersparadise.ke/wp-content/uploads/2021/04/hpc-menu-icon-1.png" 
+  },
+  { 
+    name: "Hays Suite Hotels",
+    image_url: "https://seeklogo.com/images/H/hays-plc-logo-AA3733CFEF-seeklogo.com.png" 
+  },
+  { 
+    name: "Villa Ameera Malindi",
+    image_url: "https://www.waterfront.co.za/wp-content/uploads/2018/04/logo_ameera.jpg" 
+  },
+  { 
+    name: "Hotel Nomad",
+    image_url: "https://www.logolynx.com/images/logolynx/5a/5af0d38a27319ad2a3f4c3ad9c373486.jpeg" 
+  },
+  { 
+    name: "Jumbo Club Watamu",
+    image_url: "https://www.africanresortinternational.co.ke/wp-content/uploads/2016/09/THE-ONE-Logo_4.png" 
+  },
+  { 
+    name: "Raha Suites",
+    image_url: "https://www.rahaintl.org/wp-content/uploads/2020/09/Raha-Logo-289x300.png" 
+  }
+
+])
+
+reviews = Review.create([
+  {
+    title:'Great Hotel',
+    description:'Enjoyed my stay',
+    score: 5,
+    hotel: hotels.first
+  },
+  {
+    title:'Super Experience',
+    description:'Awesome food',
+    score: 4,
+    hotel: hotels.last
+  },
+  {
+    title:'Excellent staff',
+    description:'Super services',
+    score: 4,
+    hotel: hotels.first
+  },
+  {
+    title: 'Awesome',
+    description:'This is the place to be',
+    score: 4,
+    hotel: hotels.last
+  }
+])
+
+puts 'Seeded!'
